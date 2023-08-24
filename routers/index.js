63063532310329b325034ffe763e52455ea5dc6e
Hendra <hendraahaihendra@gmail.com>
2023-08-24 21:10:19 +0700

    feat:Done feature CRD on images entity

--- routers/index.js
+++ routers/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const userRouter = require('./users');
 const adminRouter = require('./admin');
 const categoryRouter = require('./category');
+const imagesRouter = require('./images');
 
 router
   .use(userRouter)
   .use(adminRouter)
-  .use(categoryRouter);
+  .use(categoryRouter)
+  .use(imagesRouter);
 
 module.exports = router
 
