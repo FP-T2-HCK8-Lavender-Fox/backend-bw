ff4b627bb953951c7ad8cfe8bf96fc06ac0eff9b
poncosh <vhonchosatrio@gmail.com>
2023-08-29 12:42:35 +0700

    feat: calculation point

--- controllers/checkpointController.js
+++ controllers/checkpointController.js
@@ -1,5 +1,10 @@
-const { Checkpoint, sequelize, AnswerQuiz } = require("../models");
-const QRCode = require('qrcode');
+const {
+  Checkpoint,
+  sequelize,
+  AnswerQuiz,
+  User_Event,
+  Event,
+} = require("../models");
 
 module.exports = class checkpointController {
   static getAllCheckpoint = async (req, res, next) => {
@@ -17,10 +22,9 @@ module.exports = class checkpointController {
       const { eventId } = req.params;
       const dataCategory = await Checkpoint.findAll({
         where: {
-          EventId: eventId
-        }
+          EventId: eventId,
+        },
       });
-      if (dataCategory.length === 0) throw ({ name: "Data not found!" })
       res.status(200).json(dataCategory);
     } catch (error) {
       console.log(error);
@@ -29,19 +33,70 @@ module.exports = class checkpointController {
   };
 
   static answerCheckpoint = async (req, res, next) => {
+    const t = await sequelize.transaction();
     try {
       const { CheckpointId } = req.params;
       const { answer } = req.body;
-      if (!CheckpointId) throw ({ name: "CheckpointId is required!" })
-      await AnswerQuiz.create({
-        trueOrFalse: answer,
-        CheckpointId,
-        UserId: req.user.id
+      const question = await Checkpoint.findByPk(CheckpointId, {
+        include: {
+          model: Event,
+        },
       });
+      let eventStartDate = new Date(question.Event.startDate);
+      const timeStartDate = new Date(question.Event.startDate).getTime();
+      const userEvent = await User_Event.findOne({
+        where: {
+          EventId: question.EventId,
+          UserId: req.user.id,
+        },
+      });
+      if (question.trueAnswer === answer) {
+        await AnswerQuiz.update(
+          {
+            trueOrFalse: true,
+          },
+          {
+            where: {
+              CheckpointId: CheckpointId,
+              UserId: req.user.id,
+            },
+            transaction: t,
+          }
+        );
 
-      res.status(201).json({ message: "success post answer data" });
+        const midnightDate = eventStartDate.setUTCHours(0, 0, 0, 0);
+        const eventStart = timeStartDate - midnightDate;
+        const now = new Date().getTime() - midnightDate;
+
+        const point = Math.floor((eventStart / now) * 1000000);
+
+        await userEvent.update({
+          point: null ? point : this.point + point,
+        });
+
+        await userEvent.save({ transaction: t });
+
+        await t.commit();
+        return res.status(200).json(`User answer is true`);
+      } else {
+        await AnswerQuiz.update(
+          {
+            trueOrFalse: false,
+          },
+          {
+            where: {
+              CheckpointId: CheckpointId,
+              UserId: req.user.id,
+            },
+            transaction: t,
+          }
+        );
+
+        await t.commit();
+        return res.status(200).json(`User answer is false`);
+      }
     } catch (error) {
-      console.log(error);
+      await t.rollback();
       next(error);
     }
   };
